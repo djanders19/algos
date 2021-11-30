ea7b090c56e3ab6126a300b4e7f53cbe06bc213f
david <davidias.j.a@gmail.com>
2021-11-30 09:14:51 -0500

    updated tests

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import functions as f
 import HillClimbingAgent as hc
 import RandomRestartAgent as rr
+import SimulatedAnnealingAgent as sa
 import random as rand
 rosenbrock_minimum = [1,1]
 rastrigin_ackley_sphere_minimum = [0,0]
@@ -40,9 +41,11 @@ print(num2)
 num3 = d.eval(rastrigin_ackley_sphere_minimum)
 print(num3)
 
+
+# Hill-Climbing Tests:
 print("\n\nNOW TESTING HILL-CLIMBING")
 print("ROSENBROCK:")
-state = [rand.uniform(-5, 10) for _ in range(a.dim)]
+state = [rand.uniform(-2.048, 2.048) for _ in range(a.dim)]
 agent = hc.HillClimbingAgent(a, state, 0.01)
 agent.print_state()
 agent.run()
@@ -74,10 +77,11 @@ print("\nBEST SPHERE SOLUTION FOUND:")
 agent.print_state()
 
 
+# RR-Hill Climbing Tests:
 print("\n\nNOW TESTING RANDOM-RESTART HILL-CLIMBING")
 print("ROSENBROCK:")
-state = [rand.uniform(-5, 10) for _ in range(a.dim)]
-agent = rr.RandomRestartAgent(b, state, 0.01, 10, 5.0)
+state = [rand.uniform(-2.048, 2.048) for _ in range(a.dim)]
+agent = rr.RandomRestartAgent(a, state, 0.01, 10, 2.048)
 agent.print_state()
 agent.run()
 print("\nBEST ROSENBROCK SOLUTION FOUND:")
@@ -85,7 +89,7 @@ agent.print_state()
 
 print("\n\nRASTRIGIN:")
 state = [rand.uniform(-5.12, 5.12) for _ in range(b.dim)]
-agent = rr.RandomRestartAgent(b, state, 0.01, 10, 5.0)
+agent = rr.RandomRestartAgent(b, state, 0.01, 10, 5.12)
 agent.print_state()
 agent.run()
 print("\nBEST RASTRIGIN SOLUTION FOUND:")
@@ -93,7 +97,7 @@ agent.print_state()
 
 print("\n\nACKLEY:")
 state = [rand.uniform(-32.768, 32.768) for _ in range(c.dim)]
-agent = rr.RandomRestartAgent(b, state, 0.01, 10, 5.0)
+agent = rr.RandomRestartAgent(c, state, 0.01, 10, 32.768)
 agent.print_state()
 agent.run()
 print("\nBEST ACKLEY SOLUTION FOUND:")
@@ -101,7 +105,44 @@ agent.print_state()
 
 print("\n\nSPHERE:")
 state = [rand.uniform(-5.12, 5.12) for _ in range(d.dim)]
-agent = rr.RandomRestartAgent(b, state, 0.01, 10, 5.0)
+agent = rr.RandomRestartAgent(d, state, 0.01, 10, 5.12)
+agent.print_state()
+agent.run()
+print("\nBEST SPHERE SOLUTION FOUND:")
+agent.print_state()
+
+
+# Simulated annealing tests:
+print("\n\nNOW TESTING SIMULATED ANNEALING")
+schedule = [x / 1000.0 for x in range(25000, 0, -1)]
+
+print("ROSENBROCK:")
+state = [rand.uniform(-2.048, 2.048) for _ in range(a.dim)]
+agent = sa.SimulatedAnnealingAgent(a, state, 0.1, schedule)
+agent.print_state()
+agent.run()
+print("\nBEST ROSENBROCK SOLUTION FOUND:")
+agent.print_state()
+
+print("\n\nRASTRIGIN:")
+state = [rand.uniform(-5.12, 5.12) for _ in range(b.dim)]
+agent = sa.SimulatedAnnealingAgent(b, state, 0.1, schedule)
+agent.print_state()
+agent.run()
+print("\nBEST RASTRIGIN SOLUTION FOUND:")
+agent.print_state()
+
+print("\n\nACKLEY:")
+state = [rand.uniform(-32.768, 32.768) for _ in range(c.dim)]
+agent = sa.SimulatedAnnealingAgent(c, state, 0.1, schedule)
+agent.print_state()
+agent.run()
+print("\nBEST ACKLEY SOLUTION FOUND:")
+agent.print_state()
+
+print("\n\nSPHERE:")
+state = [rand.uniform(-5.12, 5.12) for _ in range(d.dim)]
+agent = sa.SimulatedAnnealingAgent(d, state, 0.1, schedule)
 agent.print_state()
 agent.run()
 print("\nBEST SPHERE SOLUTION FOUND:")
